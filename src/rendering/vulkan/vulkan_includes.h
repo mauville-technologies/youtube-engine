89cd75111889c22cc0a463f7db83b64cc4dac6f2
Paul Mauviel <pmauviel@imvu.com>
2022-02-11 17:51:24 -0500

    Forgot to check for clang compiler

--- src/rendering/vulkan/vulkan_includes.h
+++ src/rendering/vulkan/vulkan_includes.h
@@ -7,9 +7,15 @@
 
 #include <vulkan/vulkan.h>
 
-// This file is riddled with compiler warnings that we want to suppress. So we move our vulkan includes into here.
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Weverything"
+#ifdef __clang__
+    // This file is riddled with compiler warnings that we want to suppress. So we move our vulkan includes into here.
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Weverything"
+#endif
+
 #include <vk_mem_alloc.h>
-#pragma clang diagnostic pop
+
+#ifdef __clang__
+    #pragma clang diagnostic pop
+#endif
