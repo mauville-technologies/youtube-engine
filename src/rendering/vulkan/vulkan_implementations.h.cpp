b8cbeaf1ce6284e2c9fdda58868fbf37a6ca31a2
Paul Mauviel <ozzadar@ozzadar.com>
2023-02-01 23:46:36 -0500

    Remove glob from CMakeLists to improve IDE performance

--- src/rendering/vulkan/vulkan_implementations.h.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by ozzadar on 2022-02-11.
-// Copyright (c) 2021 Mauville Technologies Incorporated. All rights reserved.
-//
-
-#define VMA_IMPLEMENTATION
-#include "vulkan_includes.h"
